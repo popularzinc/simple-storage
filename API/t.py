e0b52fa96b6883327b2e5a6095a9f847fc653f8d
popularzinc <93499654+popularzinc@users.noreply.github.com>
2022-09-01 22:38:39 -0500

    Update t.py

--- API/t.py
+++ API/t.py
@@ -2,6 +2,14 @@ import StorageAPI
 
 server = StorageAPI.API()
 
-filename = 'Data-1.jpeg'
+filename = 'test.txt'
 
-server.PutFile(filename,filename='test.jpeg')
+## Get list of files and folders in directory
+list = server.GetList(dir='/')
+
+## Uploads file to server
+server.PutFile(filename,filename='test2.txt')
+
+## Downloads file and returns file object
+file = server.GetFile('test2.txt')
+file.save("downloaded.txt")
